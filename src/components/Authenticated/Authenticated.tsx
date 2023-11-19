9cc9d7d40d4d39918659cb96caaacd9bf7f6c848
Krog <amaydura59@gmail.com>
2023-11-19 18:29:04 +0100

    retrieve token

--- src/components/Authenticated/Authenticated.tsx
+++ src/components/Authenticated/Authenticated.tsx
@@ -1,4 +1,5 @@
 import { messages } from "@/constants/messages";
+import { retrieveToken } from "@/services/auth/auth";
 import { selectAuthState, setAuthState } from "@/store/reducers/authReducers";
 import { message } from "antd";
 import getConfig from "next/config";
@@ -17,15 +18,30 @@ const Authenticated = ({children}: Props) => {
     const router = useRouter();
     const authState = useSelector(selectAuthState);
 
+    const errorRedirect = () => {
+        const href = window.location.href;
+        message.error(messages.login.error.general);
+        router.push(publicRuntimeConfig.APP_NAME + 'login?r' + href, undefined, {shallow: true});;
+    }
+
     useEffect(() => {
         if(typeof window !== 'undefined'){
             try {
+                retrieveToken().then(
+                    (token) => {
+                        if(token) {
+                            if(!authState) message.success(messages.login.success(publicRuntimeConfig.APP_NAME));
+                            dispatch(setAuthState(true));
 
+                            //get user profile
+                        } else {
+                            errorRedirect();
+                        }
+                    }
+                )
             } catch(e) {
                 dispatch(setAuthState(false));
-                const href = window.location.href;
-                message.error(messages.login.error.general);
-                router.push(publicRuntimeConfig.APP_NAME + 'login?r' + href, undefined, {shallow: true});
+                errorRedirect();
             }
         }
     }, [dispatch, router])
